02791adf62eeefc3ad73afda24da28694cbbf8f1
hey-rae <1171849689@qq.com>
2019-11-21 21:26:41 +0800

    修改FileURlUtil工具类

--- fitness-utils/src/main/java/com/woniu/fitness/utils/FileUrlUtil.java
+++ fitness-utils/src/main/java/com/woniu/fitness/utils/FileUrlUtil.java
@@ -2,6 +2,7 @@ package com.woniu.fitness.utils;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
 
 /**
  * 功能描述:<br>
@@ -27,5 +28,28 @@ public class FileUrlUtil {
             e.printStackTrace();
             return "请求失败";
         }
+    }
+
+    //判断是图片还是视频
+    public static String judgeFile(String filePath){
+        System.out.println(filePath);
+        String[] images={"jpg","jpeg","png","gif","bmp"};
+        String[] videos={"mp4","avi","mov","rmvb","rm","wmn","mkv","mlv"};
+        String[] str=filePath.split("\\.");
+        System.out.println(str.length);
+        //文件后缀名
+        String format=str[str.length-1];
+        System.out.println(format);
+        for(String img:images){
+            if(img.equals(format)){
+                return "图片";
+            }
+        }
+        for(String video:videos){
+            if(video.equals(format)){
+                return "视频";
+            }
+        }
+        return "错误类型";
     }
 }
