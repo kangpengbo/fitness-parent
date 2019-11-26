8cddb9ba241983979f0622fc1636f30b441a9e28
jack <2713429311@qq.com>
2019-11-26 08:46:16 +0800

    redis session 的配置文件

---
+++ fitness-item-9000/src/main/java/com/woniu/fitness/redisSession/RedisSessionConfig/RedisSessionConfig.java
@@ -0,0 +1,17 @@
+package com.woniu.fitness.redisSession.RedisSessionConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * 功能描述:<br>
+ * 〈〉
+ *
+ * @author FeianLing
+ * @create 2019/11/23
+ * @since 1.0.0
+ */
+@Configuration
+@EnableRedisHttpSession
+public class RedisSessionConfig {
+}
