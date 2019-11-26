9067c9c17310ea021d601311212b4546c0b608e1
wt <wt@woniu.com>
2019-11-26 21:00:52 +0800

    1

--- fitness-item-9000/src/main/java/com/woniu/fitness/service/ArticleServiceImpl.java
+++ fitness-item-9000/src/main/java/com/woniu/fitness/service/ArticleServiceImpl.java
@@ -7,10 +7,8 @@ import org.springframework.data.annotation.Transient;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @Service
 @Transactional
