07481fd72449659bb3ff41511225e36f58756f70
jack <2713429311@qq.com>
2019-11-26 08:48:20 +0800

    配置启动类

--- fitness-item-9000/src/main/java/com/woniu/fitness/Item_App9000.java
+++ fitness-item-9000/src/main/java/com/woniu/fitness/Item_App9000.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -18,6 +19,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableEurekaClient
 @EnableTransactionManagement
 @MapperScan("com.woniu.fitness.mapper")
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60 * 30)
 public class Item_App9000 {
     public static void main(String[] args) {
         SpringApplication.run(Item_App9000.class,args);
